41599496ff3040031ad223ec89782728ce301d78
Asifat Kazeem Adeniyi <asifatkazeem@yahoo.com>
2020-01-21 22:52:51 +0100

    Final Update

---
+++ about-us.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Lixir | Official Page</title>
+    <script src="js/jquery-1.11.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+    <link rel="stylesheet" href="css/about-us.css">
+
+</head>
+
+<body>
+    <?php require_once "./Fragments/header.php" ?>
+    <section class="content">
+        <div class="about">
+            <h1>About Lixir</h1>
+        </div>
+        <div class="profile">
+            <h2 class="">Company Profile</h3>
+                <p class="">
+                    Lixir is technology first company with a group of agile talents from diverse backgrounds as UX/UI, Digital Marketing, Web, Mobile and Software Devs, IOT devs, AR, VR devs & content creators.
+                </p>
+                <p >We are a company poised with three things at it core;
+                    <ol class="core-values">
+                        <li>Innovative tech solutions to enhance the way we live, work & play</li>
+                        <li>Optimize/disrupt business operations to enhance growth and </li>
+                        <li>Reduce/eliminate friction between businesses and people.</li>
+                    </ol>
+                </p>
+                <p> We are a glocal team with technological innovation and human expertise adding value to the
+                    burgeoning human needs. </p>
+                <aside class="card">
+                    <h3>Our Goal</h3>
+                    <p class="">Our goal is to build a community of talents that will create valuable solution oriented
+                        products for people and businesses to thrive.</p>
+                </aside>
+
+                <aside class="card">
+                    <h3>Our Vision</h3>
+                    <p> Augment the standard of living and ease of doing busines in the continent with the
+                        use technology. </p>
+                </aside>
+        </div>
+        <div class="services">
+            <h1>What We Do</h1>
+            <div class="cen">
+                <div class="service">
+                    <img src="./images/surface1.png" alt="">
+                    <h2>Software Development</h2>
+                    <hr class="title">
+
+                    <p>
+                        We are all about software solutions,
+                        we are highly skilled in the business
+                        of creating Internet-based applications,
+                        mobile applications,consultancy services,
+                        systems integration. We believe in world-
+                        class software solutions that suit
+                        organizational backlogs.
+
+                    </p>
+                    <hr class="rule">
+
+                </div>
+                <div class="service">
+                    <img src="./images/data 1.png" alt="">
+                    <h2>Website Development</h2>
+                    <hr class="title">
+
+                    <p>
+                        Responsive and mobile web design
+                        Web app UI design
+                        Company and product sites
+                        Website reviews
+                        <span>
+                            mobile applications,consultancy services,
+                            systems integration. We believe in world-
+                            class software solutions that suit
+                            organizational backlogs.
+                        </span>
+
+                    </p>
+                    <hr class="rule">
+
+                </div>
+                <div class="service">
+                    <img src="./images/presentation 1.png" alt="">
+                    <h2>Training & Support</h2>
+                    <hr class="title">
+
+                    <p>
+
+                        We train and support newbie developers
+                        to become giants in the tech space, in an
+                        effort to help build their dreams.
+                        <span>
+                            mobile applications,consultancy services,
+                            systems integration. We believe in world-
+                            class software solutions that suit
+                            organizational backlogs.
+                        </span>
+
+                    </p>
+                    <hr class="rule">
+
+                </div>
+                <div class="service">
+                    <img src="./images/Frame 1.png" alt="">
+                    <h2>Content</h2>
+                    <hr class="title">
+
+                    <p>
+                        Content strategy,
+                        Digital copywriting
+                        Tone of voice. Content reviews.
+                        <span>
+                            mobile applications,consultancy services,
+                            systems integration. We believe in world-
+                            class software solutions that suit
+                            organizational backlogs.
+                            class software solutions that suit
+                        </span>
+
+
+
+                    </p>
+                    <hr class="rule">
+
+                </div>
+            </div>
+        </div>
+        <div class="section">
+            <h2>Take your business forward</h2>
+            <a href="project-request.php"><button>Start A Project</button></a>
+            <hr>
+        </div>
+    </section>
+
+    <?php require_once "./Fragments/footer.php" ?>
+
+</body>
+
+</html>
