b8cd7fe1e18a8f8deb2b54a0f99a77191ddbd382
Phiiileo <emmanuelboluwajib1@gmail.com>
2020-02-06 08:10:30 +0100

    fix bug in js file

--- js/main.js
+++ js/main.js
@@ -8,102 +8,25 @@ $(".log_details_container h4").on("mouseleave", function () {
     $(this).next().css("height", "0")
 })
 
+
+
+
+
 let init = 0;
 let slide_stack = $(".carousel1_slide").length;
 
 function automateSlider() {
     let browserWidth = document.body.clientWidth;
-    if (browserWidth < 770) {//hover effect on log details
-
-        $(".log_details_container h4").on("mouseenter", function () {
-            $(this).next().css("height", "30px")
-        })
-
-        $(".log_details_container h4").on("mouseleave", function () {
-            $(this).next().css("height", "0")
-        })
-
-        let init = 0;
-        let slide_stack = $(".carousel1_slide").length;
-
-        function automateSlider() {
-            let browserWidth = document.body.clientWidth;
-            if (browserWidth < 770) {
-                return
-            }
-            else {
-                if (init >= slide_stack - 1) {
-                    $(".control--right").css("display", "none")
-                    return
-                }
-                else {
-                    $(".control--left").css("display", "block")
-
-                    let active_slide = $(".carousel1 .active");
-                    let next_slide = active_slide.next();
-                    active_slide.css("left", "-100%").removeClass("active");
-                    next_slide.addClass("active");
-                    init++
-                }
-            }
-        }
-        setInterval(automateSlider, 100);
-
-
-
-
-
-        //control banner slider
-        $(".control").click(function () {
-            let active_slide = $(".carousel1 .active");
-            let next_slide = active_slide.next();
-            let prev_slide = active_slide.prev();
-
-            //left btn control
-            if ($(this).hasClass("control--left")) {
-                if (init === 0) { $(this).css("display", "none") }
-                else {
-                    active_slide.css("left", "100%").removeClass("active")
-                    prev_slide.addClass("active");
-                    init--
-                    $(".control--right").css("display", "block")
-
-                }
-            }
-            //right btn control
-            else {
-                if (init >= slide_stack - 1) { $(this).css("display", "none") }
-                else {
-                    active_slide.css("left", "-100%").removeClass("active");
-                    next_slide.addClass("active");
-                    init++
-                    $(".control--left").css("display", "block")
-                }
-            }
-        })
-
-        // Control team slider
-        $(".controlT").click(function () {
-            let obj = $(".team-carousel")[0];
-            if ($(this).hasClass("control_left")) {
-                obj.scrollLeft -= 200;
-            }
-            else {
-                obj.scrollLeft += 200;
-            }
-        })
-
-        setInterval(() => {
-            let obj = $(".team-carousel")[0];
-            obj.scrollLeft += 300;
-        }, 10000);
+    if (browserWidth < 600) {
         return
     }
     else {
         if (init >= slide_stack - 1) {
+            $(".control--right").css("display", "none")
             return
         }
         else {
+            $(".control--left").css("display", "block")
             let active_slide = $(".carousel1 .active");
             let next_slide = active_slide.next();
             active_slide.css("left", "-100%").removeClass("active");
@@ -112,7 +35,9 @@ function automateSlider() {
         }
     }
 }
-// setInterval(automateSlider, 100);
+
+
+setInterval(automateSlider, 8000);
 
 
 //banner slider
@@ -127,6 +52,8 @@ $(".control").click(function () {
         else {
             active_slide.css("left", "100%").removeClass("active")
             prev_slide.addClass("active");
+            $(".control--right").css("display", "block")
+
             init--
         }
     }
@@ -158,7 +85,6 @@ $(".controlT").click(function () {
 setInterval(() => {
     let obj = $(".team-carousel")[0];
     obj.scrollLeft += 300;
-    console.log(obj.offsetWidth)
 }, 10000);
 
 //Added animation
@@ -169,6 +95,5 @@ document.body.onscroll = () => {
    $(box[1]).addClass("animateLeft")
    $(box[2]).addClass("animateRight")
    $(box[3]).addClass("animateTop")
-    console.log(window.pageYOffset)
     }
 }
